dcfd1507666d5e1ced029b65eaef47fe565b138b
Nicole L <dlegare.1001@gmail.com>
2023-03-27 14:26:31 -0700

    Move TODO comment

--- src/arm/cpu.rs
+++ src/arm/cpu.rs
@@ -9,7 +9,6 @@ pub const NEON_HWCAP: libc::c_ulong = 1 << 12;
 pub unsafe extern "C" fn dav1d_get_cpu_flags_arm() -> libc::c_uint {
     let mut flags = 0;
 
-    // TODO: Support Android by parsing `/proc/cpuinfo` the way the original C does.
     cfg_if! {
         if #[cfg(any(
             target_arch = "aarch64",
@@ -18,6 +17,7 @@ pub unsafe extern "C" fn dav1d_get_cpu_flags_arm() -> libc::c_uint {
         ))] {
             flags |= DAV1D_ARM_CPU_FLAG_NEON;
         } else if #[cfg(target_os = "android")] {
+            // TODO: Support Android by parsing `/proc/cpuinfo` the way the original C does.
             todo!("Android is not yet supported")
         } else {
             if (libc::getauxval(libc::AT_HWCAP) & NEON_HWCAP) != 0 {
