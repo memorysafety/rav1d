99dbe8d6a3cf3af6e2cbd551b37bb411d877cef4
Khyber Sen <kkysen@gmail.com>
2023-03-31 23:24:20 -0700

    Deduplicate `struct BlockContext` to `src/env.rs`.

--- src/lib.rs
+++ src/lib.rs
@@ -1343,27 +1343,7 @@ pub struct refmvs_frame {
     pub n_tile_threads: libc::c_int,
     pub n_frame_threads: libc::c_int,
 }
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct BlockContext {
-    pub mode: [uint8_t; 32],
-    pub lcoef: [uint8_t; 32],
-    pub ccoef: [[uint8_t; 32]; 2],
-    pub seg_pred: [uint8_t; 32],
-    pub skip: [uint8_t; 32],
-    pub skip_mode: [uint8_t; 32],
-    pub intra: [uint8_t; 32],
-    pub comp_type: [uint8_t; 32],
-    pub ref_0: [[int8_t; 32]; 2],
-    pub filter: [[uint8_t; 32]; 2],
-    pub tx_intra: [int8_t; 32],
-    pub tx: [int8_t; 32],
-    pub tx_lpf_y: [uint8_t; 32],
-    pub tx_lpf_uv: [uint8_t; 32],
-    pub partition: [uint8_t; 16],
-    pub uvmode: [uint8_t; 32],
-    pub pal_sz: [uint8_t; 32],
-}
+use crate::src::env::BlockContext;
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub struct Dav1dTileState {
