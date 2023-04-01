51262a019638c3f95fb4d4285442e2d8b2c7639a
Khyber Sen <kkysen@gmail.com>
2023-04-01 02:41:16 -0700

    Deduplicate/move `struct _IO_FILE` to `include/stdio.rs`.

--- src/lib.rs
+++ src/lib.rs
@@ -168,8 +168,8 @@ extern "C" {
 }
 
 
-use crate::include::sys::types::__off_t;
-use crate::include::sys::types::__off64_t;
+
+
 use crate::include::dav1d::common::Dav1dUserData;
 use crate::src::r#ref::Dav1dRef;
 use crate::include::stdatomic::atomic_int;
@@ -1678,39 +1678,7 @@ use crate::src::levels::BL_128X128;
 use crate::include::pthread::pthread_condattr_t;
 use crate::include::pthread::pthread_mutexattr_t;
 pub type FILE = _IO_FILE;
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct _IO_FILE {
-    pub _flags: libc::c_int,
-    pub _IO_read_ptr: *mut libc::c_char,
-    pub _IO_read_end: *mut libc::c_char,
-    pub _IO_read_base: *mut libc::c_char,
-    pub _IO_write_base: *mut libc::c_char,
-    pub _IO_write_ptr: *mut libc::c_char,
-    pub _IO_write_end: *mut libc::c_char,
-    pub _IO_buf_base: *mut libc::c_char,
-    pub _IO_buf_end: *mut libc::c_char,
-    pub _IO_save_base: *mut libc::c_char,
-    pub _IO_backup_base: *mut libc::c_char,
-    pub _IO_save_end: *mut libc::c_char,
-    pub _markers: *mut _IO_marker,
-    pub _chain: *mut _IO_FILE,
-    pub _fileno: libc::c_int,
-    pub _flags2: libc::c_int,
-    pub _old_offset: __off_t,
-    pub _cur_column: libc::c_ushort,
-    pub _vtable_offset: libc::c_schar,
-    pub _shortbuf: [libc::c_char; 1],
-    pub _lock: *mut libc::c_void,
-    pub _offset: __off64_t,
-    pub _codecvt: *mut _IO_codecvt,
-    pub _wide_data: *mut _IO_wide_data,
-    pub _freeres_list: *mut _IO_FILE,
-    pub _freeres_buf: *mut libc::c_void,
-    pub __pad5: size_t,
-    pub _mode: libc::c_int,
-    pub _unused2: [libc::c_char; 20],
-}
+use crate::include::stdio::_IO_FILE;
 
 use crate::include::pthread::pthread_once_t;
 
