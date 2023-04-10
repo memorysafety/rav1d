0abbdbb3436f8fd7ccb4f0693d08e363e34fb27d
Khyber Sen <kkysen@gmail.com>
2023-04-10 15:38:38 -0700

    Deduplicate `struct Dav1dPicture` to `include/dav1d/picture.rs`.

--- include/dav1d/picture.rs
+++ include/dav1d/picture.rs
@@ -1,4 +1,13 @@
+use crate::include::dav1d::common::Dav1dDataProps;
+use crate::include::dav1d::headers::Dav1dContentLightLevel;
+use crate::include::dav1d::headers::Dav1dFrameHeader;
+use crate::include::dav1d::headers::Dav1dITUTT35;
+use crate::include::dav1d::headers::Dav1dMasteringDisplay;
 use crate::include::dav1d::headers::Dav1dPixelLayout;
+use crate::include::dav1d::headers::Dav1dSequenceHeader;
+use crate::include::stddef::ptrdiff_t;
+use crate::include::stdint::uintptr_t;
+use crate::src::r#ref::Dav1dRef;
 
 #[derive(Copy, Clone)]
 #[repr(C)]
@@ -7,4 +16,27 @@ pub struct Dav1dPictureParameters {
     pub h: libc::c_int,
     pub layout: Dav1dPixelLayout,
     pub bpc: libc::c_int,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct Dav1dPicture {
+    pub seq_hdr: *mut Dav1dSequenceHeader,
+    pub frame_hdr: *mut Dav1dFrameHeader,
+    pub data: [*mut libc::c_void; 3],
+    pub stride: [ptrdiff_t; 2],
+    pub p: Dav1dPictureParameters,
+    pub m: Dav1dDataProps,
+    pub content_light: *mut Dav1dContentLightLevel,
+    pub mastering_display: *mut Dav1dMasteringDisplay,
+    pub itut_t35: *mut Dav1dITUTT35,
+    pub reserved: [uintptr_t; 4],
+    pub frame_hdr_ref: *mut Dav1dRef,
+    pub seq_hdr_ref: *mut Dav1dRef,
+    pub content_light_ref: *mut Dav1dRef,
+    pub mastering_display_ref: *mut Dav1dRef,
+    pub itut_t35_ref: *mut Dav1dRef,
+    pub reserved_ref: [uintptr_t; 4],
+    pub ref_0: *mut Dav1dRef,
+    pub allocator_data: *mut libc::c_void,
 }
--- src/recon_tmpl_8.rs
+++ src/recon_tmpl_8.rs
@@ -266,32 +266,11 @@ pub struct C2RustUnnamed_1 {
     pub grain_lut_8bpc: [[[int8_t; 82]; 74]; 3],
     pub scaling_8bpc: [[uint8_t; 256]; 3],
 }
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct Dav1dPicture {
-    pub seq_hdr: *mut Dav1dSequenceHeader,
-    pub frame_hdr: *mut Dav1dFrameHeader,
-    pub data: [*mut libc::c_void; 3],
-    pub stride: [ptrdiff_t; 2],
-    pub p: Dav1dPictureParameters,
-    pub m: Dav1dDataProps,
-    pub content_light: *mut Dav1dContentLightLevel,
-    pub mastering_display: *mut Dav1dMasteringDisplay,
-    pub itut_t35: *mut Dav1dITUTT35,
-    pub reserved: [uintptr_t; 4],
-    pub frame_hdr_ref: *mut Dav1dRef,
-    pub seq_hdr_ref: *mut Dav1dRef,
-    pub content_light_ref: *mut Dav1dRef,
-    pub mastering_display_ref: *mut Dav1dRef,
-    pub itut_t35_ref: *mut Dav1dRef,
-    pub reserved_ref: [uintptr_t; 4],
-    pub ref_0: *mut Dav1dRef,
-    pub allocator_data: *mut libc::c_void,
-}
+use crate::include::dav1d::picture::Dav1dPicture;
 use crate::include::dav1d::headers::Dav1dITUTT35;
 use crate::include::dav1d::headers::Dav1dMasteringDisplay;
 use crate::include::dav1d::headers::Dav1dContentLightLevel;
-use crate::include::dav1d::picture::Dav1dPictureParameters;
+
 use crate::include::dav1d::headers::Dav1dPixelLayout;
 use crate::include::dav1d::headers::DAV1D_PIXEL_LAYOUT_I444;
 
