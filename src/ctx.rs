81e056c7d9d3672edc477797fb3ee54c860a5304
Khyber Sen <kkysen@gmail.com>
2023-04-14 02:05:13 -0700

    Port the `case_set` C macro to an efficient Rust function using a closure and `AlignN` types.

--- src/ctx.rs
+++ src/ctx.rs
@@ -1,7 +1,10 @@
+use std::ffi::c_int;
+
 use crate::include::stdint::uint16_t;
 use crate::include::stdint::uint32_t;
 use crate::include::stdint::uint64_t;
 use crate::include::stdint::uint8_t;
+use crate::src::align::{Align1, Align16, Align2, Align32, Align4, Align8};
 
 #[derive(Copy, Clone)]
 #[repr(C)]
@@ -28,4 +31,51 @@ pub union alias32 {
 pub union alias64 {
     pub u64_0: uint64_t,
     pub u8_0: [uint8_t; 8],
+}
+
+pub struct CaseSetter {
+    align: u8,
+    offset: u32,
+}
+
+impl CaseSetter {
+    unsafe fn set_impl(&self, buf: *mut u8, multiplier: u8) {
+        let buf = buf.offset(self.offset as isize);
+        debug_assert!(buf as usize & ((self.align as usize) - 1) == 0);
+        // This compiles to more efficient assembly than the dav1d C approach naively ported to Rust.
+        // For example, this way allows Rust to optimize better
+        // and use 16-byte moves instead of always 8-byte moves at most
+        // (or whatever is best for the architecture).
+        // The array val repeats are compiled identically,
+        // so the single, repeated `0x01` mask here is a lot simpler.
+        // If performance isn't the most important; it's a lot simpler code, too.
+        let val = 0x01 * multiplier;
+        match self.align {
+            1 => *buf.cast() = Align1([val; 1]),
+            2 => *buf.cast() = Align2([val; 2]),
+            4 => *buf.cast() = Align4([val; 4]),
+            8 => *buf.cast() = Align8([val; 8]),
+            16 => *buf.cast() = Align16([val; 16]),
+            32 => *buf.cast() = Align32([val; 32]),
+            _ => {}
+        };
+    }
+
+    pub unsafe fn set(&self, buf: &mut [u8], multiplier: impl TryInto<u8>) {
+        self.set_impl(
+            buf.as_mut_ptr(),
+            multiplier.try_into().unwrap_unchecked(), // a generic `as` cast
+        );
+    }
+}
+
+pub unsafe fn case_set<T, F>(align: c_int, offset: c_int, dir: &mut T, set_ctx: F)
+where
+    F: Fn(&CaseSetter, &mut T),
+{
+    let case = CaseSetter {
+        align: align as u8,
+        offset: offset as u32,
+    };
+    set_ctx(&case, dir);
 }
