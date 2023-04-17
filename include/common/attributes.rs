9b847b9baecdc397e92b64ce677629b6a2f849eb
Khyber Sen <kkysen@gmail.com>
2023-04-17 00:34:56 -0700

    Make `include/common/attributes.rs` already safe `fn`s nominally safe.

--- include/common/attributes.rs
+++ include/common/attributes.rs
@@ -1,14 +1,14 @@
 #[inline]
-pub unsafe extern "C" fn ctz(mask: libc::c_uint) -> libc::c_int {
+pub fn ctz(mask: libc::c_uint) -> libc::c_int {
     return mask.trailing_zeros() as i32;
 }
 
 #[inline]
-pub unsafe extern "C" fn clz(mask: libc::c_uint) -> libc::c_int {
+pub fn clz(mask: libc::c_uint) -> libc::c_int {
     return mask.leading_zeros() as i32;
 }
 
 #[inline]
-pub unsafe extern "C" fn clzll(mask: libc::c_ulonglong) -> libc::c_int {
+pub fn clzll(mask: libc::c_ulonglong) -> libc::c_int {
     return mask.leading_zeros() as i32;
 }
