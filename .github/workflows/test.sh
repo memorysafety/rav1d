da2b6c4e0ced0679abe73dd58c05cf28fa0880f4
Per Larsen <perl@immunant.com>
2023-04-18 16:29:29 -0700

    Revert commits accidentally pushed to master

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,19 +4,16 @@ set -eu
 set -o pipefail
 
 timeout_multiplier=1
-rust_test_path=../target/release/dav1d
-while getopts t:r: flag
+while getopts t: flag
 do
     case "${flag}" in
         t) timeout_multiplier=${OPTARG};;
-        r) rust_test_path=${OPTARG};;
     esac
 done
 
 mkdir -p build && pushd build
 
-# --reconfigure is necessary in case $rust_test_path changed
-meson setup --reconfigure -Dbuild_rust=false -Dtest_rust_path=$rust_test_path ..
+meson setup ..
 meson test --no-rebuild \
     --suite testdata-8 \
     --suite testdata-10 \
