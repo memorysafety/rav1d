c2f60763355277c1c91326ab1d5e0f9344dfbd86
Per Larsen <perl@immunant.com>
2023-04-18 17:46:18 -0700

    Rename and simplify arm7 CI config

---
+++ .github/workflows/arm7/envsetup.sh
@@ -0,0 +1,4 @@
+export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
+export CC_armv7_unknown_linux_gnueabihf=arm-linux-gnueabihf-gcc
+export CXX_armv7_unknown_linux_gnueabihf=arm-linux-gnueabihf-g++
+export RUSTFLAGS="-C target-feature=+crt-static"
