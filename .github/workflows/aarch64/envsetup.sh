88139a2d153ddc9a338602c530f719d413ad4ba9
Per Larsen <perl@immunant.com>
2023-04-19 15:57:54 -0700

    Move target-specific config into .cargo/config.toml

--- .github/workflows/aarch64/envsetup.sh
+++
@@ -1,4 +0,0 @@
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
-export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
-export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
-export RUSTFLAGS="-C target-feature=+crt-static"
