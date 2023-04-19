37ec2ac507d21ddcf06f1c2bc0aac21c30ff74d0
Khyber Sen <kkysen@gmail.com>
2023-04-19 15:08:01 -0700

    `fn fix_int_mv_precision`: Make `mv` arg a ref (propagated to `fn fix_mv_precision`, too).

--- src/env.rs
+++ src/env.rs
@@ -77,29 +77,29 @@ pub unsafe extern "C" fn get_poc_diff(
 }
 
 #[inline]
-unsafe fn fix_int_mv_precision(mv: *mut mv) {
-    (*mv)
-        .x = (((*mv).x as libc::c_int
-        - ((*mv).x as libc::c_int >> 15 as libc::c_int)
+unsafe fn fix_int_mv_precision(mv: &mut mv) {
+    mv
+        .x = ((mv.x as libc::c_int
+        - (mv.x as libc::c_int >> 15 as libc::c_int)
         + 3 as libc::c_int) as libc::c_uint & !(7 as libc::c_uint)) as int16_t;
-    (*mv)
-        .y = (((*mv).y as libc::c_int
-        - ((*mv).y as libc::c_int >> 15 as libc::c_int)
+    mv
+        .y = ((mv.y as libc::c_int
+        - (mv.y as libc::c_int >> 15 as libc::c_int)
         + 3 as libc::c_int) as libc::c_uint & !(7 as libc::c_uint)) as int16_t;
 }
 
 #[inline]
-pub unsafe extern "C" fn fix_mv_precision(hdr: *const Dav1dFrameHeader, mv: *mut mv) {
+pub unsafe extern "C" fn fix_mv_precision(hdr: *const Dav1dFrameHeader, mv: &mut mv) {
     if (*hdr).force_integer_mv != 0 {
         fix_int_mv_precision(mv);
     } else if (*hdr).hp == 0 {
-        (*mv)
-            .x = (((*mv).x as libc::c_int
-            - ((*mv).x as libc::c_int >> 15 as libc::c_int))
+        mv
+            .x = ((mv.x as libc::c_int
+            - (mv.x as libc::c_int >> 15 as libc::c_int))
             as libc::c_uint & !(1 as libc::c_uint)) as int16_t;
-        (*mv)
-            .y = (((*mv).y as libc::c_int
-            - ((*mv).y as libc::c_int >> 15 as libc::c_int))
+        mv
+            .y = ((mv.y as libc::c_int
+            - (mv.y as libc::c_int >> 15 as libc::c_int))
             as libc::c_uint & !(1 as libc::c_uint)) as int16_t;
     }
 }
