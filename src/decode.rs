cf15abf1aa6e711879affd25d8ed639ad74de35e
Khyber Sen <kkysen@gmail.com>
2023-04-20 23:34:38 -0700

    `static dav1d_filter_dir`: Deduplicate declarations.

--- src/decode.rs
+++ src/decode.rs
@@ -47,7 +47,6 @@ extern "C" {
     );
     fn dav1d_data_props_copy(dst: *mut Dav1dDataProps, src: *const Dav1dDataProps);
     fn dav1d_data_unref_internal(buf: *mut Dav1dData);
-    static dav1d_filter_dir: [[uint8_t; 2]; 10];
     static dav1d_intra_mode_context: [uint8_t; 13];
     static dav1d_wedge_ctx_lut: [uint8_t; 22];
     static dav1d_sgr_params: [[uint16_t; 2]; 16];
@@ -302,6 +301,7 @@ use crate::src::tables::dav1d_block_dimensions;
 use crate::src::tables::dav1d_block_sizes;
 use crate::src::tables::dav1d_comp_inter_pred_modes;
 use crate::src::tables::dav1d_filter_2d;
+use crate::src::tables::dav1d_filter_dir;
 use crate::src::tables::dav1d_max_txfm_size_for_bs;
 use crate::src::tables::dav1d_partition_type_count;
 use crate::src::tables::dav1d_txfm_dimensions;
