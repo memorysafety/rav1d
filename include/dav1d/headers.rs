9030736b778a2b8fa027425b86c9857902b7894d
Khyber Sen <kkysen@gmail.com>
2023-04-26 00:04:14 -0700

    `fn dav1d_get_shear_params`: Use standard Rust int types.

--- include/dav1d/headers.rs
+++ include/dav1d/headers.rs
@@ -1,6 +1,4 @@
 use crate::include::stddef::size_t;
-use crate::include::stdint::int16_t;
-use crate::include::stdint::int32_t;
 use crate::include::stdint::int8_t;
 use crate::include::stdint::uint16_t;
 use crate::include::stdint::uint32_t;
@@ -43,27 +41,31 @@ pub const DAV1D_WM_TYPE_AFFINE: Dav1dWarpedMotionType = 3;
 pub const DAV1D_WM_TYPE_ROT_ZOOM: Dav1dWarpedMotionType = 2;
 pub const DAV1D_WM_TYPE_TRANSLATION: Dav1dWarpedMotionType = 1;
 pub const DAV1D_WM_TYPE_IDENTITY: Dav1dWarpedMotionType = 0;
+
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub struct Dav1dWarpedMotionParams_u_p {
-    pub alpha: int16_t,
-    pub beta: int16_t,
-    pub gamma: int16_t,
-    pub delta: int16_t,
+    pub alpha: i16,
+    pub beta: i16,
+    pub gamma: i16,
+    pub delta: i16,
 }
+
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub union Dav1dWarpedMotionParams_u {
     pub p: Dav1dWarpedMotionParams_u_p,
-    pub abcd: [int16_t; 4],
+    pub abcd: [i16; 4],
 }
+
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub struct Dav1dWarpedMotionParams {
     pub type_0: Dav1dWarpedMotionType,
-    pub matrix: [int32_t; 6],
+    pub matrix: [i32; 6],
     pub u: Dav1dWarpedMotionParams_u,
 }
+
 pub type Dav1dPixelLayout = libc::c_uint;
 pub const DAV1D_PIXEL_LAYOUT_I444: Dav1dPixelLayout = 3;
 pub const DAV1D_PIXEL_LAYOUT_I422: Dav1dPixelLayout = 2;
--- src/warpmv.rs
+++ src/warpmv.rs
@@ -55,29 +55,23 @@ pub unsafe fn dav1d_get_shear_params(wm: &mut Dav1dWarpedMotionParams) -> libc::
     if mat[2] <= 0 {
         return 1;
     }
-    wm.u.p.alpha = iclip_wmp(mat[2] - 0x10000) as int16_t;
-    wm.u.p.beta = iclip_wmp(mat[3]) as int16_t;
+    wm.u.p.alpha = iclip_wmp(mat[2] - 0x10000) as i16;
+    wm.u.p.beta = iclip_wmp(mat[3]) as i16;
     let (mut shift, y) = resolve_divisor_32((mat[2]).abs() as u32);
     let y = apply_sign(y, mat[2]);
-    let v1 = mat[4] as int64_t * 0x10000 * y as int64_t;
+    let v1 = mat[4] as i64 * 0x10000 * y as i64;
     let rnd = 1 << shift >> 1;
     wm.u.p.gamma = iclip_wmp(apply_sign64(
-        (v1.abs() + rnd as libc::c_longlong >> shift) as libc::c_int,
+        (v1.abs() + rnd as i64 >> shift) as libc::c_int,
         v1,
-    )) as int16_t;
-    let v2 = mat[3] as int64_t * mat[4] as int64_t * y as int64_t;
+    )) as i16;
+    let v2 = mat[3] as i64 * mat[4] as i64 * y as i64;
     wm.u.p.delta = iclip_wmp(
-        mat[5]
-            - apply_sign64(
-                (v2.abs() + rnd as libc::c_longlong >> shift) as libc::c_int,
-                v2,
-            )
-            - 0x10000,
-    ) as int16_t;
-    return (4 * (wm.u.p.alpha as libc::c_int).abs() + 7 * (wm.u.p.beta as libc::c_int).abs()
-        >= 0x10000
-        || 4 * (wm.u.p.gamma as libc::c_int).abs() + 4 * (wm.u.p.delta as libc::c_int).abs()
-            >= 0x10000) as libc::c_int;
+        mat[5] - apply_sign64((v2.abs() + rnd as i64 >> shift) as libc::c_int, v2) - 0x10000,
+    ) as i16;
+    return (4 * (wm.u.p.alpha as i32).abs() + 7 * (wm.u.p.beta as i32).abs() >= 0x10000
+        || 4 * (wm.u.p.gamma as i32).abs() + 4 * (wm.u.p.delta as i32).abs() >= 0x10000)
+        as libc::c_int;
 }
 
 fn resolve_divisor_64(d: u64) -> (libc::c_int, libc::c_int) {
