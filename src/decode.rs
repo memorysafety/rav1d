1b46c3f5eaab91a254abb424272a5923d461afd5
Khyber Sen <kkysen@gmail.com>
2023-04-30 19:51:52 -0700

    `fn get_tx_ctx`: Standardize some of the internally casted types.

--- src/decode.rs
+++ src/decode.rs
@@ -1138,8 +1138,8 @@ fn get_tx_ctx(
     yb4: libc::c_int,
     xb4: libc::c_int,
 ) -> libc::c_int {
-    (l.tx_intra[yb4 as usize] as libc::c_int >= max_tx.lh as libc::c_int) as libc::c_int
-        + (a.tx_intra[xb4 as usize] as libc::c_int >= max_tx.lw as libc::c_int) as libc::c_int
+    (l.tx_intra[yb4 as usize] as i32 >= max_tx.lh as i32) as libc::c_int
+        + (a.tx_intra[xb4 as usize] as i32 >= max_tx.lw as i32) as libc::c_int
 }
 
 #[inline]
