f4c3f22673efc0af9bd9ddd9fd93749c120bd7b4
Khyber Sen <kkysen@gmail.com>
2023-05-08 15:56:12 -0700

    `fn read_mv_residual`: Make `t` arg a ref.

--- src/decode.rs
+++ src/decode.rs
@@ -1211,29 +1211,29 @@ unsafe fn read_mv_component_diff(
 }
 
 unsafe fn read_mv_residual(
-    t: *mut Dav1dTaskContext,
+    t: &mut Dav1dTaskContext,
     ref_mv: *mut mv,
     mv_cdf: *mut CdfMvContext,
     have_fp: bool,
 ) {
     match dav1d_msac_decode_symbol_adapt4(
-        &mut (*(*t).ts).msac,
-        &mut (*(*t).ts).cdf.mv.joint.0,
+        &mut (*t.ts).msac,
+        &mut (*t.ts).cdf.mv.joint.0,
         (N_MV_JOINTS as libc::c_int - 1) as size_t,
     ) {
         3 => {
-            (*ref_mv).y = (*ref_mv).y
-                + read_mv_component_diff(&mut *t, &mut (*mv_cdf).comp[0], have_fp) as i16;
-            (*ref_mv).x = (*ref_mv).x
-                + read_mv_component_diff(&mut *t, &mut (*mv_cdf).comp[1], have_fp) as i16;
+            (*ref_mv).y =
+                (*ref_mv).y + read_mv_component_diff(t, &mut (*mv_cdf).comp[0], have_fp) as i16;
+            (*ref_mv).x =
+                (*ref_mv).x + read_mv_component_diff(t, &mut (*mv_cdf).comp[1], have_fp) as i16;
         }
         1 => {
-            (*ref_mv).x = (*ref_mv).x
-                + read_mv_component_diff(&mut *t, &mut (*mv_cdf).comp[1], have_fp) as i16;
+            (*ref_mv).x =
+                (*ref_mv).x + read_mv_component_diff(t, &mut (*mv_cdf).comp[1], have_fp) as i16;
         }
         2 => {
-            (*ref_mv).y = (*ref_mv).y
-                + read_mv_component_diff(&mut *t, &mut (*mv_cdf).comp[0], have_fp) as i16;
+            (*ref_mv).y =
+                (*ref_mv).y + read_mv_component_diff(t, &mut (*mv_cdf).comp[0], have_fp) as i16;
         }
         _ => {}
     };
