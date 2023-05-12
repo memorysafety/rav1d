e7110b2d97a4398fc08459d84634576bd606bfa7
Khyber Sen <kkysen@gmail.com>
2023-05-11 22:55:58 -0700

    `fn read_vartx_tree`: Remove type annotation from `tx_split` var.

--- src/decode.rs
+++ src/decode.rs
@@ -1940,7 +1940,7 @@ unsafe fn read_vartx_tree(
     let b_dim = &dav1d_block_dimensions[bs as usize];
     let bw4 = b_dim[0] as usize;
     let bh4 = b_dim[1] as usize;
-    let mut tx_split: [u16; 2] = [0, 0];
+    let mut tx_split = [0u16; 2];
     *b.max_ytx_mut() = dav1d_max_txfm_size_for_bs[bs as usize][0];
     let txfm_mode = (*f.frame_hdr).txfm_mode as Dav1dTxfmMode;
     if b.skip == 0
