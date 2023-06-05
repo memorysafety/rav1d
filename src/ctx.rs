2c3276a76c25b9d7eb6a6779de139933628a3a8d
Khyber Sen <kkysen@gmail.com>
2023-06-05 15:30:03 -0700

    `fn CaseSetter::set`: Use `.fill`/`memset` instead of `small_memset` (1.687 s instead of 1.642 s on `chimera_8b_1080p.ivf -l 1000`).

--- src/ctx.rs
+++ src/ctx.rs
@@ -1,32 +1,5 @@
 use std::iter::zip;
 
-#[inline]
-fn small_memset<T: Clone + Copy, const UP_TO: usize, const WITH_DEFAULT: bool>(
-    buf: &mut [T],
-    val: T,
-) {
-    macro_rules! set {
-        ($n:literal) => {{
-            let buf: &mut [T; $n] = buf.try_into().unwrap();
-            *buf = [val; $n];
-        }};
-    }
-    match buf.len() {
-        1 if UP_TO >= 1 => set!(1),
-        2 if UP_TO >= 2 => set!(2),
-        4 if UP_TO >= 4 => set!(4),
-        8 if UP_TO >= 8 => set!(8),
-        16 if UP_TO >= 16 => set!(16),
-        32 if UP_TO >= 32 => set!(32),
-        64 if UP_TO >= 64 => set!(64),
-        _ => {
-            if WITH_DEFAULT {
-                buf.fill(val)
-            }
-        }
-    }
-}
-
 pub struct CaseSetter<const UP_TO: usize, const WITH_DEFAULT: bool> {
     offset: usize,
     len: usize,
@@ -35,7 +8,7 @@ pub struct CaseSetter<const UP_TO: usize, const WITH_DEFAULT: bool> {
 impl<const UP_TO: usize, const WITH_DEFAULT: bool> CaseSetter<UP_TO, WITH_DEFAULT> {
     #[inline]
     pub fn set<T: Clone + Copy>(&self, buf: &mut [T], val: T) {
-        small_memset::<T, UP_TO, WITH_DEFAULT>(&mut buf[self.offset..][..self.len], val);
+        buf[self.offset..][..self.len].fill(val);
     }
 }
 
