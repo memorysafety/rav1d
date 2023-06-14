79bfc2f0bbfbc461a468728b2a45093816866b88
Khyber Sen <kkysen@gmail.com>
2023-06-13 22:50:36 -0700

    `fn ac_dump`: Translate `for` loops.

--- include/common/dump.rs
+++ include/common/dump.rs
@@ -3,21 +3,14 @@ use crate::include::stdint::int16_t;
 #[inline]
 pub unsafe fn ac_dump(
     mut buf: *const int16_t,
-    mut w: libc::c_int,
-    mut h: libc::c_int,
+    w: libc::c_int,
+    h: libc::c_int,
     what: &str,
 ) {
     println!("{}", what);
-    loop {
-        let fresh1 = h;
-        h = h - 1;
-        if !(fresh1 != 0) {
-            break;
-        }
-        let mut x = 0;
-        while x < w {
+    for _ in 0..h {
+        for x in 0..w {
             print!(" {:03}", *buf.offset(x as isize));
-            x += 1;
         }
         buf = buf.offset(w as isize);
         println!();
