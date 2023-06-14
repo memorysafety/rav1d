19a49f81f2d5e3829375e1b03b030dbcd6b6106c
Khyber Sen <kkysen@gmail.com>
2023-06-13 23:03:30 -0700

    `fn ac_dump`: Make `buf` an array ref.

--- include/common/dump.rs
+++ include/common/dump.rs
@@ -1,5 +1,5 @@
 #[inline]
-pub fn ac_dump(mut buf: &[i16], w: usize, h: usize, what: &str) {
+pub fn ac_dump(mut buf: &[i16; 32 * 32], w: usize, h: usize, what: &str) {
     println!("{}", what);
     for buf in buf.chunks_exact(w).take(h) {
         for x in buf {
