44658cfed862ffd127be9937ae7f27cd8d00de6e
Khyber Sen <kkysen@gmail.com>
2023-06-21 00:05:17 -0700

    `fn prep_c`: Make `src` a slice internally based on its accessed `len`.

--- src/mc.rs
+++ src/mc.rs
@@ -11,13 +11,13 @@ pub unsafe fn prep_c<BD: BitDepth>(
     bd: BD,
 ) {
     let mut tmp = std::slice::from_raw_parts_mut(tmp, w * h);
+    let mut src = std::slice::from_raw_parts(src, src_stride * h);
     let intermediate_bits = bd.get_intermediate_bits();
     for _ in 0..h {
         for x in 0..w {
-            tmp[x] = (((*src.offset(x as isize)).as_::<i32>() << intermediate_bits)
-                - (BD::PREP_BIAS as i32)) as i16;
+            tmp[x] = ((src[x].as_::<i32>() << intermediate_bits) - (BD::PREP_BIAS as i32)) as i16;
         }
         tmp = &mut tmp[w..];
-        src = src.offset(src_stride as isize);
+        src = &src[src_stride..];
     }
 }
