4c0e20385c63edb7488e9caf96a90e26345ccd45
Khyber Sen <kkysen@gmail.com>
2023-08-28 14:39:35 -0700

    `fn pthread_cond_broadcast`: Deduplicate declarations into `use` imports.

--- src/lib.rs
+++ src/lib.rs
@@ -62,7 +62,6 @@ extern "C" {
     ) -> libc::c_int;
     fn pthread_cond_wait(__cond: *mut pthread_cond_t, __mutex: *mut pthread_mutex_t)
         -> libc::c_int;
-    fn pthread_cond_broadcast(__cond: *mut pthread_cond_t) -> libc::c_int;
     fn pthread_cond_destroy(__cond: *mut pthread_cond_t) -> libc::c_int;
     fn dav1d_log(c: *mut Dav1dContext, format: *const libc::c_char, _: ...);
     fn dav1d_parse_obus(
@@ -171,6 +170,7 @@ use crate::src::mem::Dav1dMemPool;
 use libc::pthread_attr_destroy;
 use libc::pthread_attr_init;
 use libc::pthread_attr_setstacksize;
+use libc::pthread_cond_broadcast;
 use libc::pthread_mutex_t;
 
 use crate::include::dav1d::dav1d::Dav1dDecodeFrameType;
