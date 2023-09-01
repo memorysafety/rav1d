99d7bd1259650ab4b541249a464fb75299b797e2
Khyber Sen <kkysen@gmail.com>
2023-09-01 15:43:14 -0700

    `fn dav1d_submit_frame_error`: Make `f.tile` a slice before iterating over it.

--- src/decode.rs
+++ src/decode.rs
@@ -5919,8 +5919,8 @@ unsafe extern "C" fn dav1d_submit_frame_error(
     dav1d_ref_dec(&mut f.seq_hdr_ref);
     dav1d_ref_dec(&mut f.frame_hdr_ref);
     dav1d_data_props_copy(&mut c.cached_error_props, &mut c.in_0.m);
-    for i in 0..f.n_tile_data {
-        dav1d_data_unref_internal(&mut (*(f.tile).offset(i as isize)).data);
+    for tile in slice::from_raw_parts_mut(f.tile, f.n_tile_data.try_into().unwrap()) {
+        dav1d_data_unref_internal(&mut tile.data);
     }
     f.n_tile_data = 0;
     if c.n_fc > 1 {
