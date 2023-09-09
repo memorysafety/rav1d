2f55e7013089d1fd8554e9bf0f82e57c6b8d720f
Khyber Sen <kkysen@gmail.com>
2023-09-08 22:40:05 -0700

    `lib.rs`: Only make `mod`s `pub` if they need to be.

--- lib.rs
+++ lib.rs
@@ -28,103 +28,103 @@ pub mod include {
     pub mod stdatomic;
     pub mod stddef;
     pub mod stdint;
-    pub mod stdio;
+    mod stdio;
 } // mod include
 pub mod src {
-    pub mod align;
-    pub mod cdef;
+    mod align;
+    mod cdef;
     #[cfg(feature = "bitdepth_16")]
-    pub mod cdef_apply_tmpl_16;
+    mod cdef_apply_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod cdef_apply_tmpl_8;
+    mod cdef_apply_tmpl_8;
     #[cfg(feature = "bitdepth_16")]
-    pub mod cdef_tmpl_16;
+    mod cdef_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod cdef_tmpl_8;
-    pub mod cdf;
+    mod cdef_tmpl_8;
+    mod cdf;
     pub mod cpu;
-    pub mod ctx;
-    pub mod cursor;
-    pub mod data;
-    pub mod decode;
-    pub mod dequant_tables;
-    pub mod env;
+    mod ctx;
+    mod cursor;
+    mod data;
+    mod decode;
+    mod dequant_tables;
+    mod env;
     #[cfg(feature = "bitdepth_16")]
-    pub mod fg_apply_tmpl_16;
+    mod fg_apply_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod fg_apply_tmpl_8;
-    pub mod filmgrain;
+    mod fg_apply_tmpl_8;
+    mod filmgrain;
     #[cfg(feature = "bitdepth_16")]
-    pub mod filmgrain_tmpl_16;
+    mod filmgrain_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod filmgrain_tmpl_8;
-    pub mod getbits;
-    pub mod internal;
-    pub mod intra_edge;
-    pub mod ipred;
-    pub mod ipred_prepare;
+    mod filmgrain_tmpl_8;
+    mod getbits;
+    mod internal;
+    mod intra_edge;
+    mod ipred;
+    mod ipred_prepare;
     #[cfg(feature = "bitdepth_16")]
-    pub mod ipred_prepare_tmpl_16;
+    mod ipred_prepare_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod ipred_prepare_tmpl_8;
-    pub mod ipred_tmpl;
+    mod ipred_prepare_tmpl_8;
+    mod ipred_tmpl;
     #[cfg(feature = "bitdepth_16")]
-    pub mod ipred_tmpl_16;
+    mod ipred_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod ipred_tmpl_8;
-    pub mod itx;
-    pub mod itx_1d;
+    mod ipred_tmpl_8;
+    mod itx;
+    mod itx_1d;
     #[cfg(feature = "bitdepth_16")]
-    pub mod itx_tmpl_16;
-    pub mod itx_tmpl_8;
-    pub mod levels;
+    mod itx_tmpl_16;
+    mod itx_tmpl_8;
+    mod levels;
     #[cfg(feature = "bitdepth_16")]
-    pub mod lf_apply_tmpl_16;
+    mod lf_apply_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod lf_apply_tmpl_8;
-    pub mod lf_mask;
+    mod lf_apply_tmpl_8;
+    mod lf_mask;
     pub mod lib;
-    pub mod log;
-    pub mod loopfilter;
+    mod log;
+    mod loopfilter;
     #[cfg(feature = "bitdepth_16")]
-    pub mod loopfilter_tmpl_16;
+    mod loopfilter_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod loopfilter_tmpl_8;
-    pub mod looprestoration;
-    pub mod lr_apply;
+    mod loopfilter_tmpl_8;
+    mod looprestoration;
+    mod lr_apply;
     #[cfg(feature = "bitdepth_16")]
-    pub mod lr_apply_tmpl_16;
+    mod lr_apply_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod lr_apply_tmpl_8;
-    pub mod mc;
+    mod lr_apply_tmpl_8;
+    mod mc;
     #[cfg(feature = "bitdepth_16")]
-    pub mod mc_tmpl_16;
+    mod mc_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod mc_tmpl_8;
-    pub mod mem;
-    pub mod msac;
-    pub mod obu;
-    pub mod picture;
-    pub mod qm;
-    pub mod recon;
+    mod mc_tmpl_8;
+    mod mem;
+    mod msac;
+    mod obu;
+    mod picture;
+    mod qm;
+    mod recon;
     #[cfg(feature = "bitdepth_16")]
-    pub mod recon_tmpl_16;
+    mod recon_tmpl_16;
     #[cfg(feature = "bitdepth_8")]
-    pub mod recon_tmpl_8;
+    mod recon_tmpl_8;
     pub mod r#ref;
-    pub mod refmvs;
-    pub mod scan;
-    pub mod tables;
-    pub mod thread_data;
-    pub mod thread_task;
-    pub mod warpmv;
-    pub mod wedge;
+    mod refmvs;
+    mod scan;
+    mod tables;
+    mod thread_data;
+    mod thread_task;
+    mod warpmv;
+    mod wedge;
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-    pub mod x86 {
+    mod x86 {
         pub mod cpu;
     } // mod x86
     #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
-    pub mod arm {
+    mod arm {
         pub mod cpu;
     } // mod arm
 } // mod src
@@ -133,8 +133,8 @@ pub mod src {
 cfg_if::cfg_if! {
     if #[cfg(target_os = "linux")] {
         extern "C" {
-            pub static mut stdout: *mut libc::FILE;
-            pub static mut stderr: *mut libc::FILE;
+            static mut stdout: *mut libc::FILE;
+            static mut stderr: *mut libc::FILE;
         }
 
         unsafe fn errno_location() -> *mut libc::c_int {
