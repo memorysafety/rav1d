e3cb40b101aa5223d0c82e2712fd704ea4eb0d93
Khyber Sen <kkysen@gmail.com>
2023-09-11 03:37:32 -0700

    `fn build_nondc_ii_masks`: `const`ify by passing and returning array by value.
    Also, use an `if` `else` instead of `cmp::min` and a loop instead of `.fill`.

--- src/wedge.rs
+++ src/wedge.rs
@@ -1,5 +1,3 @@
-use std::cmp;
-
 use crate::src::align::Align16;
 use crate::src::align::Align32;
 use crate::src::align::Align64;
@@ -580,26 +578,29 @@ pub static dav1d_ii_masks: [[[Option<&'static [u8]>; N_INTER_INTRA_PRED_MODES]; 
     masks
 };
 
-#[cold]
-fn build_nondc_ii_masks<const N: usize>(
-    masks: &mut [[u8; N]; N_II_PRED_MODES],
+const fn build_nondc_ii_masks<const N: usize>(
+    mut masks: [[u8; N]; N_II_PRED_MODES],
     w: usize,
     h: usize,
     step: usize,
-) {
-    static ii_weights_1d: [u8; 32] = [
+) -> [[u8; N]; N_II_PRED_MODES] {
+    const ii_weights_1d: [u8; 32] = [
         60, 52, 45, 39, 34, 30, 26, 22, 19, 17, 15, 13, 11, 10, 8, 7, 6, 6, 5, 4, 4, 3, 3, 2, 2, 2,
         2, 1, 1, 1, 1, 1,
     ];
 
     const_for!(y in 0..h => {
         let off = y * w;
-        masks[II_VERT_PRED as usize - 1][off..][..w].fill(ii_weights_1d[y * step]);
+        const_for!(i in 0..w => {
+            masks[II_VERT_PRED as usize - 1][off + i] = ii_weights_1d[y * step];
+        });
         const_for!(x in 0..w => {
-            masks[II_SMOOTH_PRED as usize - 1][off + x] = ii_weights_1d[cmp::min(x, y) * step];
+            masks[II_SMOOTH_PRED as usize - 1][off + x] = ii_weights_1d[if x < y { x } else { y } * step];
             masks[II_HOR_PRED as usize - 1][off + x] = ii_weights_1d[x * step];
         });
     });
+
+    masks
 }
 
 #[cold]
@@ -607,13 +608,13 @@ pub unsafe fn dav1d_init_interintra_masks() {
     // This function is guaranteed to be called only once
 
     memset(ii_dc_mask.0.as_mut_ptr() as *mut libc::c_void, 32, 32 * 32);
-    build_nondc_ii_masks(&mut ii_nondc_mask_32x32.0, 32, 32, 1);
-    build_nondc_ii_masks(&mut ii_nondc_mask_16x32.0, 16, 32, 1);
-    build_nondc_ii_masks(&mut ii_nondc_mask_16x16.0, 16, 16, 2);
-    build_nondc_ii_masks(&mut ii_nondc_mask_8x32.0, 8, 32, 1);
-    build_nondc_ii_masks(&mut ii_nondc_mask_8x16.0, 8, 16, 2);
-    build_nondc_ii_masks(&mut ii_nondc_mask_8x8.0, 8, 8, 4);
-    build_nondc_ii_masks(&mut ii_nondc_mask_4x16.0, 4, 16, 2);
-    build_nondc_ii_masks(&mut ii_nondc_mask_4x8.0, 4, 8, 4);
-    build_nondc_ii_masks(&mut ii_nondc_mask_4x4.0, 4, 4, 8);
+    ii_nondc_mask_32x32.0 = build_nondc_ii_masks(ii_nondc_mask_32x32.0, 32, 32, 1);
+    ii_nondc_mask_16x32.0 = build_nondc_ii_masks(ii_nondc_mask_16x32.0, 16, 32, 1);
+    ii_nondc_mask_16x16.0 = build_nondc_ii_masks(ii_nondc_mask_16x16.0, 16, 16, 2);
+    ii_nondc_mask_8x32.0 = build_nondc_ii_masks(ii_nondc_mask_8x32.0, 8, 32, 1);
+    ii_nondc_mask_8x16.0 = build_nondc_ii_masks(ii_nondc_mask_8x16.0, 8, 16, 2);
+    ii_nondc_mask_8x8.0 = build_nondc_ii_masks(ii_nondc_mask_8x8.0, 8, 8, 4);
+    ii_nondc_mask_4x16.0 = build_nondc_ii_masks(ii_nondc_mask_4x16.0, 4, 16, 2);
+    ii_nondc_mask_4x8.0 = build_nondc_ii_masks(ii_nondc_mask_4x8.0, 4, 8, 4);
+    ii_nondc_mask_4x4.0 = build_nondc_ii_masks(ii_nondc_mask_4x4.0, 4, 4, 8);
 }
