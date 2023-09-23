8fdc2181917db3ef6bd7b0b55e8bdc016b792e3d
Khyber Sen <kkysen@gmail.com>
2023-09-23 02:10:49 -0700

    `fn dav1d_get_cpu_flags_arm`: Use `is_{arm,aarch64}_feature_detected!` for CPU feature detection.
    `is_arm_feature_detected!` is still unstable (https://github.com/rust-lang/rust/issues/111190),
    so we have to enable `#![feature(stdsimd)]` on `arm`,
    though it doesn't look like this part of `stdsimd` is likely to change.

--- src/arm/cpu.rs
+++ src/arm/cpu.rs
@@ -1,7 +1,6 @@
 use bitflags::bitflags;
-use cfg_if::cfg_if;
+use std::arch;
 use std::ffi::c_uint;
-use std::ffi::c_ulong;
 
 bitflags! {
     pub struct CpuFlags: c_uint {
@@ -9,27 +8,18 @@ bitflags! {
     }
 }
 
-pub const NEON_HWCAP: c_ulong = 1 << 12;
-
 #[cold]
 pub unsafe fn dav1d_get_cpu_flags_arm() -> CpuFlags {
     let mut flags = CpuFlags::empty();
 
-    cfg_if! {
-        if #[cfg(any(
-            target_arch = "aarch64",
-            target_os = "windows",
-            target_os = "macos"
-        ))] {
-            flags |= CpuFlags::NEON;
-        } else if #[cfg(target_arch = "arm")] {
-            if (libc::getauxval(libc::AT_HWCAP) & NEON_HWCAP) != 0 {
-                flags |= CpuFlags::NEON;
-            }
-        } else if #[cfg(target_os = "android")] {
-            // TODO: Support Android by parsing `/proc/cpuinfo` the way the original C does.
-            todo!("Android is not yet supported")
-        }
+    #[cfg(target_arch = "arm")]
+    if arch::is_arm_feature_detected!("neon") {
+        flags |= CpuFlags::NEON;
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    if arch::is_aarch64_feature_detected!("neon") {
+        flags |= CpuFlags::NEON;
     }
 
     flags
