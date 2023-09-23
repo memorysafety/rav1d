6b8d28becedf2ae0cf73f89f234468d8022b19aa
Khyber Sen <kkysen@gmail.com>
2023-09-23 01:39:35 -0700

    `fn dav1d_get_cpu_flags_x86`: Use `is_x86_feature_detected!` for CPU feature detection.

--- src/x86/cpu.rs
+++ src/x86/cpu.rs
@@ -3,13 +3,10 @@ use libc::memcmp;
 use std::ffi::c_char;
 use std::ffi::c_int;
 use std::ffi::c_uint;
-use std::ffi::c_ulong;
 use std::ffi::c_void;
 
 extern "C" {
     fn dav1d_cpu_cpuid(regs: *mut CpuidRegisters, leaf: c_uint, subleaf: c_uint);
-    #[cfg(target_arch = "x86_64")]
-    fn dav1d_cpu_xgetbv(xcr: c_uint) -> u64;
 }
 
 bitflags! {
@@ -53,6 +50,38 @@ pub union C2RustUnnamed_0 {
 
 #[cold]
 pub unsafe fn dav1d_get_cpu_flags_x86() -> CpuFlags {
+    let mut flags = CpuFlags::empty();
+
+    if is_x86_feature_detected!("sse2") {
+        flags |= CpuFlags::SSE2;
+    }
+    if is_x86_feature_detected!("ssse3") {
+        flags |= CpuFlags::SSSE3;
+    }
+    if is_x86_feature_detected!("sse4.1") {
+        flags |= CpuFlags::SSE41;
+    }
+    if is_x86_feature_detected!("avx2") {
+        flags |= CpuFlags::AVX2;
+    }
+    if is_x86_feature_detected!("avx512f")
+        && is_x86_feature_detected!("avx512cd")
+        && is_x86_feature_detected!("avx512bw")
+        && is_x86_feature_detected!("avx512dq")
+        && is_x86_feature_detected!("avx512vl")
+        && is_x86_feature_detected!("avx512vnni")
+        && is_x86_feature_detected!("avx512ifma")
+        && is_x86_feature_detected!("avx512vbmi")
+        && is_x86_feature_detected!("avx512vbmi2")
+        && is_x86_feature_detected!("avx512vpopcntdq")
+        && is_x86_feature_detected!("avx512bitalg")
+        && is_x86_feature_detected!("gfni")
+        && is_x86_feature_detected!("vaes")
+        && is_x86_feature_detected!("vpclmulqdq")
+    {
+        flags |= CpuFlags::AVX512ICL;
+    }
+
     let mut cpu: C2RustUnnamed_0 = C2RustUnnamed_0 {
         r: CpuidRegisters {
             eax: 0,
@@ -62,7 +91,6 @@ pub unsafe fn dav1d_get_cpu_flags_x86() -> CpuFlags {
         },
     };
     dav1d_cpu_cpuid(&mut cpu.r, 0 as c_int as c_uint, 0 as c_int as c_uint);
-    let mut flags = CpuFlags::empty();
     if cpu.c2rust_unnamed.max_leaf >= 1 as c_uint {
         let mut r: CpuidRegisters = CpuidRegisters {
             eax: 0,
@@ -75,37 +103,6 @@ pub unsafe fn dav1d_get_cpu_flags_x86() -> CpuFlags {
             .wrapping_add(r.eax >> 12 & 0xf0 as c_int as c_uint);
         let family: c_uint = (r.eax >> 8 & 0xf as c_int as c_uint)
             .wrapping_add(r.eax >> 20 & 0xff as c_int as c_uint);
-        if r.edx & 0x6008000 as c_int as c_uint == 0x6008000 as c_int as c_uint {
-            flags |= CpuFlags::SSE2;
-            if r.ecx & 0x201 as c_int as c_uint == 0x201 as c_int as c_uint {
-                flags |= CpuFlags::SSSE3;
-                if r.ecx & 0x80000 as c_int as c_uint == 0x80000 as c_int as c_uint {
-                    flags |= CpuFlags::SSE41;
-                }
-            }
-        }
-
-        // We only support >128-bit SIMD on x86-64.
-        #[cfg(target_arch = "x86_64")]
-        if r.ecx & 0x18000000 as c_int as c_uint == 0x18000000 as c_int as c_uint {
-            let xcr0: u64 = dav1d_cpu_xgetbv(0 as c_int as c_uint);
-            if xcr0 & 0x6 as c_int as c_ulong == 0x6 as c_int as c_ulong {
-                if cpu.c2rust_unnamed.max_leaf >= 7 as c_uint {
-                    dav1d_cpu_cpuid(&mut r, 7 as c_int as c_uint, 0 as c_int as c_uint);
-                    if r.ebx & 0x128 as c_int as c_uint == 0x128 as c_int as c_uint {
-                        flags |= CpuFlags::AVX2;
-                        if xcr0 & 0xe0 as c_int as c_ulong == 0xe0 as c_int as c_ulong {
-                            if r.ebx & 0xd0230000 as c_uint == 0xd0230000 as c_uint
-                                && r.ecx & 0x5f42 as c_int as c_uint == 0x5f42 as c_int as c_uint
-                            {
-                                flags |= CpuFlags::AVX512ICL;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
         if memcmp(
             (cpu.c2rust_unnamed.vendor).as_mut_ptr() as *const c_void,
             b"AuthenticAMD\0" as *const u8 as *const c_char as *const c_void,
@@ -122,5 +119,6 @@ pub unsafe fn dav1d_get_cpu_flags_x86() -> CpuFlags {
             }
         }
     }
-    return flags;
+
+    flags
 }
