3b4944b78dafbadaa8f9b6bcd55c2fbb5603838d
Khyber Sen <kkysen@gmail.com>
2023-10-02 21:14:05 -0700

    `enum Rav1dError`: Optimize a bit so that it's effectively a zero-cost abstraction.

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,10 @@ use strum::FromRepr;
 #[derive(Clone, Copy, PartialEq, Eq, FromRepr)]
 #[non_exhaustive]
 pub enum Rav1dError {
+    /// Not actually used (yet), but this forces `0` to be the niche,
+    /// which is more optimal since `0` is no error for [`Dav1dResult`].
+    _EPERM = 1,
+
     ENOENT = 2,
     EIO = 5,
     EAGAIN = 11,
@@ -20,17 +24,23 @@ pub type Rav1dResult<T = ()> = Result<T, Rav1dError>;
 pub struct Dav1dResult(pub c_int);
 
 impl From<Rav1dResult> for Dav1dResult {
+    #[inline]
     fn from(value: Rav1dResult) -> Self {
-        Dav1dResult(match value {
-            Ok(()) => 0,
-            Err(e) => -(e as u8 as c_int),
-        })
+        // Doing the `-` negation on both branches
+        // makes the code short and branchless.
+        Dav1dResult(
+            -(match value {
+                Ok(()) => 0,
+                Err(e) => e as u8 as c_int,
+            }),
+        )
     }
 }
 
 impl TryFrom<Dav1dResult> for Rav1dResult {
     type Error = Dav1dResult;
 
+    #[inline]
     fn try_from(value: Dav1dResult) -> Result<Self, Self::Error> {
         match value.0 {
             0 => Ok(Ok(())),
