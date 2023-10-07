9b89fbe3951bf963adc62217de0b1f53f9f6afb8
Khyber Sen <kkysen@gmail.com>
2023-10-06 23:47:15 -0700

    `struct NumThreads`: Make fields `usize`s.

--- src/lib.rs
+++ src/lib.rs
@@ -204,27 +204,27 @@ unsafe fn get_stack_size_internal(_thread_attr: *const pthread_attr_t) -> usize 
 }
 
 struct NumThreads {
-    n_tc: c_uint,
-    n_fc: c_uint,
+    n_tc: usize,
+    n_fc: usize,
 }
 
 #[cold]
 fn get_num_threads(s: &Rav1dSettings) -> NumThreads {
     let n_tc = if s.n_threads != 0 {
-        s.n_threads as c_uint
+        s.n_threads as usize
     } else {
-        rav1d_num_logical_processors().clamp(1, 256) as c_uint
+        rav1d_num_logical_processors().clamp(1, 256)
     };
     let n_fc = if s.max_frame_delay != 0 {
-        cmp::min(s.max_frame_delay as c_uint, n_tc)
+        cmp::min(s.max_frame_delay as usize, n_tc)
     } else {
-        cmp::min((n_tc as f64).sqrt().ceil() as c_uint, 8)
+        cmp::min((n_tc as f64).sqrt().ceil() as usize, 8)
     };
     NumThreads { n_fc, n_tc }
 }
 
 #[cold]
-pub(crate) unsafe fn rav1d_get_frame_delay(s: &Rav1dSettings) -> Rav1dResult<c_uint> {
+pub(crate) unsafe fn rav1d_get_frame_delay(s: &Rav1dSettings) -> Rav1dResult<usize> {
     validate_input!((s.n_threads >= 0 && s.n_threads <= 256, EINVAL))?;
     validate_input!((s.max_frame_delay >= 0 && s.max_frame_delay <= 256, EINVAL))?;
     let NumThreads { n_tc: _, n_fc } = get_num_threads(s);
@@ -236,7 +236,7 @@ pub(crate) unsafe fn rav1d_get_frame_delay(s: &Rav1dSettings) -> Rav1dResult<c_u
 pub unsafe extern "C" fn dav1d_get_frame_delay(s: *const Dav1dSettings) -> Dav1dResult {
     (|| {
         validate_input!((!s.is_null(), EINVAL))?;
-        rav1d_get_frame_delay(&s.read().into())
+        rav1d_get_frame_delay(&s.read().into()).map(|frame_delay| frame_delay as c_uint)
     })()
     .into()
 }
@@ -333,8 +333,8 @@ pub(crate) unsafe fn rav1d_open(c_out: &mut *mut Rav1dContext, s: &Rav1dSettings
     (*c).flush = &mut (*c).flush_mem;
     *(*c).flush = 0 as c_int;
     let NumThreads { n_tc, n_fc } = get_num_threads(s);
-    (*c).n_tc = n_tc;
-    (*c).n_fc = n_fc;
+    (*c).n_tc = n_tc as c_uint;
+    (*c).n_fc = n_fc as c_uint;
     (*c).fc = rav1d_alloc_aligned(
         ::core::mem::size_of::<Rav1dFrameContext>().wrapping_mul((*c).n_fc as usize),
         32 as c_int as usize,
