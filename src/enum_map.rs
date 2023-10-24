c4cb2ec52bcf39f021d6cff87f31915a44c26955
Khyber Sen <kkysen@gmail.com>
2023-10-24 01:38:58 -0700

    `struct EnumMap`, `enum_map!`: Add a `const`-compatible `enum` map.

---
+++ src/enum_map.rs
@@ -0,0 +1,110 @@
+use std::marker::PhantomData;
+use std::ops::Index;
+use std::ops::IndexMut;
+
+pub trait EnumKey<const N: usize>: Sized + Copy {
+    const VALUES: [Self; N];
+
+    fn as_usize(self) -> usize;
+}
+
+/// This is a `const` version of [`Default::default`].
+/// `trait` `fn`s can't be `const` (yet),
+/// but we can make this an associated `const`.
+pub trait DefaultValue {
+    const DEFAULT: Self;
+}
+
+/// A map from an `enum` key `K` to `V`s.
+/// `N` is the number of possible `enum` values.
+pub struct EnumMap<K, V, const N: usize>
+where
+    K: EnumKey<N>,
+{
+    array: [V; N],
+    _phantom: PhantomData<K>,
+}
+
+impl<K, V, const N: usize> EnumMap<K, V, N>
+where
+    K: EnumKey<N>,
+{
+    /// Create an [`EnumMap`] from an existing array
+    /// where the array's indices correspond to `K`'s values `as usize`.
+    #[allow(dead_code)]
+    pub const fn new(array: [V; N]) -> Self {
+        Self {
+            array,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<K, V, const N: usize> EnumMap<K, V, N>
+where
+    K: EnumKey<N>,
+    V: DefaultValue,
+{
+    /// Create an [`EnumMap`] with default values when `V: ` [`DefaultValue`].
+    #[allow(dead_code)]
+    pub const fn default() -> Self {
+        Self {
+            array: [V::DEFAULT; N],
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<K, V, const N: usize> Index<K> for EnumMap<K, V, N>
+where
+    K: EnumKey<N>,
+{
+    type Output = V;
+
+    fn index(&self, index: K) -> &Self::Output {
+        &self.array[index.as_usize()]
+    }
+}
+
+impl<K, V, const N: usize> IndexMut<K> for EnumMap<K, V, N>
+where
+    K: EnumKey<N>,
+{
+    fn index_mut(&mut self, index: K) -> &mut Self::Output {
+        &mut self.array[index.as_usize()]
+    }
+}
+
+/// Create an [`EnumMap`] where `V: ` [`DefaultValue`]
+/// using a `match` from `K` to `V`.
+///
+/// The [`DefaultValue::DEFAULT`] is not actually ever used,
+/// but needs to exist to be able to
+/// create the array safely and at `const` time.
+///
+/// [`MaybeUninit`] can do this without [`DefaultValue`]
+/// by using `unsafe` initialization,
+/// but it also doesn't yet work in `const` contexts.
+///
+/// [`MaybeUninit`]: std::mem::MaybeUninit
+#[allow(unused_macros)]
+macro_rules! enum_map {
+    ($K:ty => $V:ty; match key { $($t:tt)* }) => {{
+        use $crate::src::enum_map::EnumKey;
+        use $crate::src::enum_map::EnumMap;
+
+        let mut a = [<$V>::DEFAULT; <$K>::VALUES.len()];
+        let mut i = 0;
+        while i < <$K>::VALUES.len() {
+            let key = <$K>::VALUES[i];
+            use $K::*;
+            let value = match key { $($t)* };
+            a[key as usize] = value;
+            i += 1;
+        }
+        EnumMap::<$K, $V, { <$K>::VALUES.len() }>::new(a)
+    }};
+}
+
+#[allow(unused_imports)]
+pub(crate) use enum_map;
