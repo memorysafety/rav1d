1cfd61136950088856be9e209e1759ba2bf706e8
Per Larsen <perl@immunant.com>
2023-11-20 04:44:24 -0800

    `fn loop_filter_h_sb128uv_{c_erased,rust}`: Deduplicate w/ generics.

--- src/loopfilter_tmpl_8.rs
+++ src/loopfilter_tmpl_8.rs
@@ -3,6 +3,7 @@ use crate::include::common::bitdepth::BitDepth8;
 use crate::include::common::bitdepth::DynPixel;
 use crate::src::lf_mask::Av1FilterLUT;
 use crate::src::loopfilter::loop_filter;
+use crate::src::loopfilter::loop_filter_h_sb128uv_c_erased;
 use crate::src::loopfilter::loop_filter_h_sb128y_c_erased;
 use crate::src::loopfilter::loop_filter_v_sb128y_rust;
 use crate::src::loopfilter::Rav1dLoopFilterDSPContext;
@@ -38,60 +39,6 @@ unsafe extern "C" fn loop_filter_v_sb128y_c_erased(
         w,
         BitDepth8::new(()),
     );
-}
-
-unsafe extern "C" fn loop_filter_h_sb128uv_c_erased(
-    dst: *mut DynPixel,
-    stride: ptrdiff_t,
-    vmask: *const u32,
-    l: *const [u8; 4],
-    b4_stride: ptrdiff_t,
-    lut: *const Av1FilterLUT,
-    h: c_int,
-    _bitdepth_max: c_int,
-) {
-    loop_filter_h_sb128uv_rust(dst.cast(), stride, vmask, l, b4_stride, lut, h)
-}
-
-unsafe fn loop_filter_h_sb128uv_rust(
-    mut dst: *mut pixel,
-    stride: ptrdiff_t,
-    vmask: *const u32,
-    mut l: *const [u8; 4],
-    b4_stride: ptrdiff_t,
-    lut: *const Av1FilterLUT,
-    _h: c_int,
-) {
-    let vm: c_uint = *vmask.offset(0) | *vmask.offset(1);
-    let mut y: c_uint = 1 as c_int as c_uint;
-    while vm & !y.wrapping_sub(1 as c_int as c_uint) != 0 {
-        if vm & y != 0 {
-            let L = if (*l.offset(0))[0] as c_int != 0 {
-                (*l.offset(0))[0] as c_int
-            } else {
-                (*l.offset(-(1 as c_int) as isize))[0] as c_int
-            };
-            if !(L == 0) {
-                let H = L >> 4;
-                let E = (*lut).e[L as usize] as c_int;
-                let I = (*lut).i[L as usize] as c_int;
-                let idx = (*vmask.offset(1) & y != 0) as c_int;
-                loop_filter(
-                    dst,
-                    E,
-                    I,
-                    H,
-                    stride,
-                    1 as c_int as ptrdiff_t,
-                    4 + 2 * idx,
-                    BitDepth8::new(()),
-                );
-            }
-        }
-        y <<= 1;
-        dst = dst.offset((4 * stride) as isize);
-        l = l.offset(b4_stride as isize);
-    }
 }
 
 unsafe extern "C" fn loop_filter_v_sb128uv_c_erased(
@@ -209,7 +156,7 @@ unsafe fn loop_filter_dsp_init_arm(c: *mut Rav1dLoopFilterDSPContext) {
 pub unsafe fn rav1d_loop_filter_dsp_init_8bpc(c: *mut Rav1dLoopFilterDSPContext) {
     (*c).loop_filter_sb[0][0] = loop_filter_h_sb128y_c_erased::<BitDepth8>;
     (*c).loop_filter_sb[0][1] = loop_filter_v_sb128y_c_erased;
-    (*c).loop_filter_sb[1][0] = loop_filter_h_sb128uv_c_erased;
+    (*c).loop_filter_sb[1][0] = loop_filter_h_sb128uv_c_erased::<BitDepth8>;
     (*c).loop_filter_sb[1][1] = loop_filter_v_sb128uv_c_erased;
 
     #[cfg(feature = "asm")]
