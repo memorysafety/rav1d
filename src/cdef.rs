c849772ac886dabfdc07ab2931670e29c74585e3
Per Larsen <perl@immunant.com>
2023-11-28 02:36:00 -0800

    `fn cdef_find_dir_*`: Deduplicate w/ generics.

--- src/cdef.rs
+++ src/cdef.rs
@@ -906,3 +906,125 @@ pub(crate) unsafe extern "C" fn cdef_filter_block_8x8_c_erased<BD: BitDepth>(
         BD::from_c(bitdepth_max),
     );
 }
+
+// TODO(perl): Temporarily pub until mod is deduplicated
+pub(crate) unsafe extern "C" fn cdef_find_dir_c_erased<BD: BitDepth>(
+    img: *const DynPixel,
+    stride: ptrdiff_t,
+    var: *mut c_uint,
+    bitdepth_max: c_int,
+) -> c_int {
+    cdef_find_dir_rust(img.cast(), stride, var, BD::from_c(bitdepth_max))
+}
+
+unsafe fn cdef_find_dir_rust<BD: BitDepth>(
+    mut img: *const BD::Pixel,
+    stride: ptrdiff_t,
+    var: *mut c_uint,
+    bd: BD,
+) -> c_int {
+    let bitdepth_min_8 = 32 - clz(bd.bitdepth_max().as_::<c_uint>()) - 8;
+    let mut partial_sum_hv: [[c_int; 8]; 2] = [[0 as c_int, 0, 0, 0, 0, 0, 0, 0], [0; 8]];
+    let mut partial_sum_diag: [[c_int; 15]; 2] = [
+        [0 as c_int, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0; 15],
+    ];
+    let mut partial_sum_alt: [[c_int; 11]; 4] = [
+        [0 as c_int, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0; 11],
+        [0; 11],
+        [0; 11],
+    ];
+    let mut y = 0;
+    while y < 8 {
+        let mut x = 0;
+        while x < 8 {
+            let px = ((*img.offset(x as isize)).as_::<c_int>() >> bitdepth_min_8) - 128;
+            partial_sum_diag[0][(y + x) as usize] += px;
+            partial_sum_alt[0][(y + (x >> 1)) as usize] += px;
+            partial_sum_hv[0][y as usize] += px;
+            partial_sum_alt[1][(3 + y - (x >> 1)) as usize] += px;
+            partial_sum_diag[1][(7 + y - x) as usize] += px;
+            partial_sum_alt[2][(3 - (y >> 1) + x) as usize] += px;
+            partial_sum_hv[1][x as usize] += px;
+            partial_sum_alt[3][((y >> 1) + x) as usize] += px;
+            x += 1;
+        }
+        img = img.offset(BD::pxstride(stride as usize) as isize);
+        y += 1;
+    }
+    let mut cost: [c_uint; 8] = [0 as c_int as c_uint, 0, 0, 0, 0, 0, 0, 0];
+    let mut n = 0;
+    while n < 8 {
+        cost[2] = (cost[2]).wrapping_add(
+            (partial_sum_hv[0][n as usize] * partial_sum_hv[0][n as usize]) as c_uint,
+        );
+        cost[6] = (cost[6]).wrapping_add(
+            (partial_sum_hv[1][n as usize] * partial_sum_hv[1][n as usize]) as c_uint,
+        );
+        n += 1;
+    }
+    cost[2] = (cost[2]).wrapping_mul(105 as c_int as c_uint);
+    cost[6] = (cost[6]).wrapping_mul(105 as c_int as c_uint);
+    static div_table: [u16; 7] = [840, 420, 280, 210, 168, 140, 120];
+    let mut n_0 = 0;
+    while n_0 < 7 {
+        let d = div_table[n_0 as usize] as c_int;
+        cost[0] = (cost[0]).wrapping_add(
+            ((partial_sum_diag[0][n_0 as usize] * partial_sum_diag[0][n_0 as usize]
+                + partial_sum_diag[0][(14 - n_0) as usize]
+                    * partial_sum_diag[0][(14 - n_0) as usize])
+                * d) as c_uint,
+        );
+        cost[4] = (cost[4]).wrapping_add(
+            ((partial_sum_diag[1][n_0 as usize] * partial_sum_diag[1][n_0 as usize]
+                + partial_sum_diag[1][(14 - n_0) as usize]
+                    * partial_sum_diag[1][(14 - n_0) as usize])
+                * d) as c_uint,
+        );
+        n_0 += 1;
+    }
+    cost[0] =
+        (cost[0]).wrapping_add((partial_sum_diag[0][7] * partial_sum_diag[0][7] * 105) as c_uint);
+    cost[4] =
+        (cost[4]).wrapping_add((partial_sum_diag[1][7] * partial_sum_diag[1][7] * 105) as c_uint);
+    let mut n_1 = 0;
+    while n_1 < 4 {
+        let cost_ptr: *mut c_uint =
+            &mut *cost.as_mut_ptr().offset((n_1 * 2 + 1) as isize) as *mut c_uint;
+        let mut m = 0;
+        while m < 5 {
+            *cost_ptr = (*cost_ptr).wrapping_add(
+                (partial_sum_alt[n_1 as usize][(3 + m) as usize]
+                    * partial_sum_alt[n_1 as usize][(3 + m) as usize]) as c_uint,
+            );
+            m += 1;
+        }
+        *cost_ptr = (*cost_ptr).wrapping_mul(105 as c_int as c_uint);
+        let mut m_0 = 0;
+        while m_0 < 3 {
+            let d_0 = div_table[(2 * m_0 + 1) as usize] as c_int;
+            *cost_ptr = (*cost_ptr).wrapping_add(
+                ((partial_sum_alt[n_1 as usize][m_0 as usize]
+                    * partial_sum_alt[n_1 as usize][m_0 as usize]
+                    + partial_sum_alt[n_1 as usize][(10 - m_0) as usize]
+                        * partial_sum_alt[n_1 as usize][(10 - m_0) as usize])
+                    * d_0) as c_uint,
+            );
+            m_0 += 1;
+        }
+        n_1 += 1;
+    }
+    let mut best_dir = 0;
+    let mut best_cost: c_uint = cost[0];
+    let mut n_2 = 1;
+    while n_2 < 8 {
+        if cost[n_2 as usize] > best_cost {
+            best_cost = cost[n_2 as usize];
+            best_dir = n_2;
+        }
+        n_2 += 1;
+    }
+    *var = best_cost.wrapping_sub(cost[(best_dir ^ 4 as c_int) as usize]) >> 10;
+    return best_dir;
+}
