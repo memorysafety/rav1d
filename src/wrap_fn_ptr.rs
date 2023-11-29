7a87b590ead52aac8c1209eddaee53fbb1ec7f2a
Khyber Sen <kkysen@gmail.com>
2023-11-28 21:29:14 -0800

    `wrap_fn_ptr!`: Document.

--- src/wrap_fn_ptr.rs
+++ src/wrap_fn_ptr.rs
@@ -24,6 +24,33 @@ impl<F> WrappedFnPtr<F> {
     }
 }
 
+/// This declares a wrapper for a `fn` ptr
+/// and defines related, useful things for that `fn` ptr.
+///
+/// The API for [`wrap_fn_ptr!`] is a `fn` signature with no body.
+/// This generates a `mod` with the name of the `fn` provided that contains:
+///
+/// * `type Fn`:
+///     A [`WrappedFnPtr`] wrapping the `fn` ptr signature provided.
+///
+/// * `impl ` [`DefaultValue`] ` for Fn`:
+///     A `const`-compatible default implementation of `Fn`
+///     that just calls [`unimplemented!`].
+///     This lets `Fn` be used by [`enum_map!`] without wrapping it in an [`Option`],
+///     and removes any need for an [`Option::unwrap`] check,
+///     as the check is moved to inside the `fn` call.
+///
+/// * `decl_fn!`:
+///     A macro that, given a `fn $fn_name:ident`,
+///     declares an `extern "C" fn` with the `fn` signature provided.
+///     This macro can and is meant to be used with [`bd_fn!`].
+///
+/// This ensures that the `fn` signature is consistent between all of these
+/// and reduces the need to repeat the `fn` signature many times.
+///
+/// [`DefaultValue`]: crate::src::enum_map::DefaultValue
+/// [`enum_map!`]: crate::src::enum_map::enum_map
+/// [`bd_fn!`]: crate::include::common::bitdepth::bd_fn
 macro_rules! wrap_fn_ptr {
     ($vis:vis unsafe extern "C" fn $name:ident(
             $($arg_name:ident: $arg_ty:ty),*$(,)?
