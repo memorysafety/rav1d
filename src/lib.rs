03c4d28612420b324e0b89979a9e57cfa936de38
Khyber Sen <kkysen@gmail.com>
2023-12-15 23:51:37 -0800

    `struct Rav1d{,Frame}Context::tiles`: Mark drops with `TODO`s for when we drop properly.

--- src/lib.rs
+++ src/lib.rs
@@ -1150,7 +1150,7 @@ unsafe fn close_internal(c_out: &mut *mut Rav1dContext, flush: c_int) {
         rav1d_free_aligned((*f).ts as *mut c_void);
         rav1d_free_aligned((*f).ipred_edge[0] as *mut c_void);
         free((*f).a as *mut c_void);
-        (*f).tiles = Default::default();
+        (*f).tiles = Default::default(); // TODO(kkysen) Remove when dropped properly.
         free((*f).lf.mask as *mut c_void);
         free((*f).lf.lr_mask as *mut c_void);
         free((*f).lf.level as *mut c_void);
@@ -1181,7 +1181,7 @@ unsafe fn close_internal(c_out: &mut *mut Rav1dContext, flush: c_int) {
     for mut tile in (*c).tiles.drain(..) {
         rav1d_data_unref_internal(&mut tile.data);
     }
-    (*c).tiles = Default::default();
+    (*c).tiles = Default::default(); // TODO(kkysen) Remove when dropped properly.
     let mut n_4 = 0;
     while n_4 < 8 {
         rav1d_cdf_thread_unref(&mut *((*c).cdf).as_mut_ptr().offset(n_4 as isize));
