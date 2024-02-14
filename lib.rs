6d7474ab52edd0e28f68ae5e71924fef2c88cded
Nicole LeGare <dlegare.1001@gmail.com>
2024-02-14 09:16:17 -0800

    Remove unused feature
    Feature no longer needed now that we use stable SIMD APIs.

--- lib.rs
+++ lib.rs
@@ -2,7 +2,6 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 #![feature(c_variadic)]
-#![feature(core_intrinsics)]
 #![cfg_attr(target_arch = "arm", feature(stdsimd))]
 #![allow(clippy::all)]
 
