7a9a78b8ebf32b7f0256c1061c87db29bd2c25fd
Khyber Sen <kkysen@gmail.com>
2024-03-30 19:48:03 -0700

    `fn decode_b_inner`: Make `rr` vars copied values.

--- src/decode.rs
+++ src/decode.rs
@@ -3110,36 +3110,36 @@ unsafe fn decode_b_inner(
                 // chroma prediction
                 if is_sub8x8 {
                     if bw4 == 1 && bh4 == ss_ver {
-                        let rr = &*(*r.add(0)).offset((t.b.x - 1) as isize);
+                        let rr = (*(*r.add(0)).offset((t.b.x - 1) as isize)).0;
                         mc_lowest_px(
-                            &mut lowest_px[rr.0.r#ref.r#ref[0] as usize - 1][1],
+                            &mut lowest_px[rr.r#ref.r#ref[0] as usize - 1][1],
                             t.b.y - 1,
                             bh4,
-                            rr.0.mv.mv[0].y,
+                            rr.mv.mv[0].y,
                             ss_ver,
-                            &f.svc[rr.0.r#ref.r#ref[0] as usize - 1][1],
+                            &f.svc[rr.r#ref.r#ref[0] as usize - 1][1],
                         );
                     }
                     if bw4 == 1 {
-                        let rr = &*(*r.add(1)).offset((t.b.x - 1) as isize);
+                        let rr = (*(*r.add(1)).offset((t.b.x - 1) as isize)).0;
                         mc_lowest_px(
-                            &mut lowest_px[rr.0.r#ref.r#ref[0] as usize - 1][1],
+                            &mut lowest_px[rr.r#ref.r#ref[0] as usize - 1][1],
                             t.b.y,
                             bh4,
-                            rr.0.mv.mv[0].y,
+                            rr.mv.mv[0].y,
                             ss_ver,
-                            &f.svc[rr.0.r#ref.r#ref[0] as usize - 1][1],
+                            &f.svc[rr.r#ref.r#ref[0] as usize - 1][1],
                         );
                     }
                     if bh4 == ss_ver {
-                        let rr = &*(*r.add(0)).offset(t.b.x as isize);
+                        let rr = (*(*r.add(0)).offset(t.b.x as isize)).0;
                         mc_lowest_px(
-                            &mut lowest_px[rr.0.r#ref.r#ref[0] as usize - 1][1],
+                            &mut lowest_px[rr.r#ref.r#ref[0] as usize - 1][1],
                             t.b.y - 1,
                             bh4,
-                            rr.0.mv.mv[0].y,
+                            rr.mv.mv[0].y,
                             ss_ver,
-                            &f.svc[rr.0.r#ref.r#ref[0] as usize - 1][1],
+                            &f.svc[rr.r#ref.r#ref[0] as usize - 1][1],
                         );
                     }
                     mc_lowest_px(
