5fd5804aea9cfbe9112004aaa574e909c7d42fce
Nicole LeGare <legaren@immunant.com>
2024-05-07 10:46:14 -0700

    Cargo fmt

--- src/internal.rs
+++ src/internal.rs
@@ -939,8 +939,8 @@ pub(crate) struct Rav1dFrameData {
     pub sb_step: c_int,
     pub sr_sb128w: c_int,
     pub dq: [[[AtomicU16; 2]; 3]; RAV1D_MAX_SEGMENTS as usize], /* [RAV1D_MAX_SEGMENTS][3 plane][2 dc/ac] */
-    pub qm: [[Option<&'static [u8]>; 3]; 19],             /* [3 plane][19] */
-    pub a: Vec<BlockContext>,                             /* len = w*tile_rows */
+    pub qm: [[Option<&'static [u8]>; 3]; 19],                   /* [3 plane][19] */
+    pub a: Vec<BlockContext>,                                   /* len = w*tile_rows */
     pub rf: RefMvsFrame,
     pub jnt_weights: [[u8; 7]; 7],
     pub bitdepth_max: c_int,
