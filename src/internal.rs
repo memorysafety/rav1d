7bf3b30956f69c51552848978b36e7fa9582b749
Nicole L <dgl@immunant.com>
2024-05-14 15:49:07 -0700

    `Cf`,`AlPal`: Make into safe zerocopy-based "unions" (#1073)

--- src/internal.rs
+++ src/internal.rs
@@ -1026,16 +1026,31 @@ pub enum TileStateRef {
     Local,
 }
 
-pub struct Cf;
+/// Array of 32 * 32 coef elements (either `i16` or `i32`).
+#[derive(FromZeroes)]
+#[repr(align(64))]
+pub struct Cf([i32; 32 * 32]);
 
-impl BitDepthDependentType for Cf {
-    type T<BD: BitDepth> = Align64<[BD::Coef; 32 * 32]>;
+impl Cf {
+    pub fn select<BD: BitDepth>(&self) -> &[BD::Coef; 32 * 32] {
+        FromBytes::ref_from_prefix(AsBytes::as_bytes(&self.0)).unwrap()
+    }
+
+    pub fn select_mut<BD: BitDepth>(&mut self) -> &mut [BD::Coef; 32 * 32] {
+        FromBytes::mut_from_prefix(AsBytes::as_bytes_mut(&mut self.0)).unwrap()
+    }
 }
 
-pub struct AlPal;
+/// 4D array of pixel elements (either `u8` or `u16`).
+///
+/// Indices are `[2 a/l][32 bx/y4][3 plane][8 palette_idx]`.
+#[derive(FromZeroes)]
+pub struct AlPal([u16; 8 * 3 * 32 * 2]);
 
-impl BitDepthDependentType for AlPal {
-    type T<BD: BitDepth> = [[[[BD::Pixel; 8]; 3]; 32]; 2]; /* [2 a/l][32 bx/y4][3 plane][8 palette_idx] */
+impl AlPal {
+    pub fn select_mut<BD: BitDepth>(&mut self) -> &mut [[[[BD::Pixel; 8]; 3]; 32]; 2] {
+        FromBytes::mut_from_prefix(AsBytes::as_bytes_mut(&mut self.0)).unwrap()
+    }
 }
 
 #[derive(FromZeroes, FromBytes, AsBytes)]
@@ -1233,8 +1248,8 @@ pub(crate) struct Rav1dTaskContext {
     pub l: BlockContext,
     pub a: usize, // Offset into `f.a`
     pub rt: refmvs_tile,
-    pub cf: BitDepthUnion<Cf>,
-    pub al_pal: BitDepthUnion<AlPal>,
+    pub cf: Cf,
+    pub al_pal: AlPal,
     pub pal_sz_uv: [[u8; 32]; 2], /* [2 a/l][32 bx4/by4] */
     pub scratch: TaskContextScratch,
 
@@ -1259,8 +1274,8 @@ impl Rav1dTaskContext {
             l: Default::default(),
             a: Default::default(),
             rt: Default::default(),
-            cf: Default::default(),
-            al_pal: Default::default(),
+            cf: FromZeroes::new_zeroed(),
+            al_pal: FromZeroes::new_zeroed(),
             pal_sz_uv: Default::default(),
             scratch: FromZeroes::new_zeroed(),
             warpmv: Default::default(),
