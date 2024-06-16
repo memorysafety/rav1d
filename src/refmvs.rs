87e6591eafc06f3be7b4c7cfa9da41202a6d8239
Khyber Sen <kkysen@gmail.com>
2024-06-16 01:55:45 -0700

    `mod refmvs`: Mark `#![deny(unsafe_op_in_unsafe_fn)]`.

--- src/refmvs.rs
+++ src/refmvs.rs
@@ -1,3 +1,5 @@
+#![deny(unsafe_op_in_unsafe_fn)]
+
 use crate::include::common::intops::apply_sign;
 use crate::include::common::intops::iclip;
 use crate::include::dav1d::headers::Rav1dFrameHeader;
