6484e8587db5c27d811dada1ef89356b37d01698
Khyber Sen <kkysen@gmail.com>
2024-06-16 02:05:02 -0700

    `fn assume`: Add `# Safety` docs and `// SAFETY` comments.

--- src/assume.rs
+++ src/assume.rs
@@ -1,9 +1,16 @@
+#![deny(unsafe_op_in_unsafe_fn)]
+
 use std::hint::unreachable_unchecked;
 
 /// A stable version of [`core::intrinsics::assume`].
+///
+/// # Safety
+///
+/// `condition` must always be `true`.
 #[inline(always)]
 pub unsafe fn assume(condition: bool) {
     if !condition {
-        unreachable_unchecked();
+        // SAFETY: `condition` is `true` by the `# Safety` preconditions.
+        unsafe { unreachable_unchecked() };
     }
 }
