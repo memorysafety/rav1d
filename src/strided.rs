ddf579ca0ed5894425dd6b86151e514bef59cf33
Khyber Sen <kkysen@gmail.com>
2024-06-28 03:42:38 -0700

    `struct WithStride`: Add type that adds a `stride: isize` to a type.

--- src/strided.rs
+++ src/strided.rs
@@ -1,4 +1,6 @@
 use crate::include::common::bitdepth::BitDepth;
+use std::ops::Deref;
+use std::ops::DerefMut;
 
 pub trait Strided {
     /// Stride in number of [`u8`] bytes.
@@ -7,5 +9,30 @@ pub trait Strided {
     /// Stride in number of [`BitDepth::Pixel`]s.
     fn pixel_stride<BD: BitDepth>(&self) -> isize {
         BD::pxstride(self.stride())
+    }
+}
+
+pub struct WithStride<T> {
+    pub buf: T,
+    pub stride: isize,
+}
+
+impl<T> Strided for WithStride<T> {
+    fn stride(&self) -> isize {
+        self.stride
+    }
+}
+
+impl<T> Deref for WithStride<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.buf
+    }
+}
+
+impl<T> DerefMut for WithStride<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.buf
     }
 }
