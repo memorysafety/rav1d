dd67e1c3d79cac2838290d96343b95a4535d639b
Khyber Sen <kkysen@gmail.com>
2024-07-17 18:59:26 -0700

    `struct Unique`: Move to its own `mod`.

--- lib.rs
+++ lib.rs
@@ -56,6 +56,7 @@ pub mod src {
     pub(crate) mod relaxed_atomic;
     pub mod send_sync_non_null;
     pub(crate) mod strided;
+    pub(crate) mod unique;
     pub(crate) mod with_offset;
     pub(crate) mod wrap_fn_ptr;
     // TODO(kkysen) Temporarily `pub(crate)` due to a `pub use` until TAIT.
