853671d5ae13b63f845bda5907800f3982690478
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-05-30 13:13:26 +0200

    Apply fixes for clippy redundant_static_lifetimes lint

--- lib.rs
+++ lib.rs
@@ -38,7 +38,6 @@
     clippy::nonminimal_bool,
     clippy::partialeq_to_none,
     clippy::precedence,
-    clippy::redundant_static_lifetimes,
     clippy::uninlined_format_args,
     clippy::unnecessary_cast,
     clippy::unnecessary_mut_passed,
