984dc2d91a367a8a393485fa582b065e4e2be67a
leo030303 <59373587+leo030303@users.noreply.github.com>
2025-07-08 17:22:08 +0100

    Update src/rust_api.rs
    Co-authored-by: Khyber Sen <kkysen@gmail.com>

--- src/rust_api.rs
+++ src/rust_api.rs
@@ -216,7 +216,7 @@ pub mod dav1d {
             let slice = &*buf;
             let len = slice.len();
 
-            let mut data: Rav1dData = Rav1dData::create(len).unwrap();
+            let mut data = Rav1dData::create(len).unwrap();
             data.data = Some(c_arc::CArc::wrap(c_box::CBox::Rust(buf)).unwrap());
             if let Some(offset) = offset {
                 data.m.offset = offset as libc::off_t;
