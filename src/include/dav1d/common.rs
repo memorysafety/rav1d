4f7f206a200f800011332876be64d8b7abb7c796
Leo Ring <leoring03@gmail.com>
2025-07-08 17:55:39 +0100

    Made offset in Rav1dDataProps an i64

--- src/include/dav1d/common.rs
+++ src/include/dav1d/common.rs
@@ -45,7 +45,7 @@ pub struct Dav1dDataProps {
 pub struct Rav1dDataProps {
     pub timestamp: i64,
     pub duration: i64,
-    pub offset: libc::off_t,
+    pub offset: i64,
     pub size: usize,
     pub user_data: Rav1dUserData,
 }
